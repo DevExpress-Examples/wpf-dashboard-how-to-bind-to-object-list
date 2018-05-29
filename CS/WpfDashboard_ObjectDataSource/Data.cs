b24eb7cd98ade9ef630848cb5ed0acaea6145d75
Boris Zaitsev <bvzai@users.noreply.github.com>
2018-05-29 17:06:28 +0300

    add example content

---
+++ CS/WpfDashboard_ObjectDataSource/Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDashboard_ObjectDataSource
+{
+    public class Data
+    {
+        public string SalesPerson { get; set; }
+        public int Quantity { get; set; }
+        static Random rnd = new Random();
+        public static List<Data> CreateData()
+        {
+            List<Data> data = new List<Data>();
+            string[] salesPersons = { "Andrew Fuller", "Michael Suyama", "Robert King", "Nancy Davolio",
+                "Margaret Peacock", "Laura Callahan", "Steven Buchanan", "Janet Leverling" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                Data record = new Data();
+                //int seed = (int)DateTime.Now.Ticks & 0x0000FFFF;
+                record.SalesPerson = salesPersons[rnd.Next(0, salesPersons.Length)];
+                record.Quantity = rnd.Next(0, 100);
+                data.Add(record);
+            }
+            return data;
+        }
+    }
+}
---
+++ CS/WpfDashboard_ObjectDataSource/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using DevExpress.DashboardCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDashboard_ObjectDataSource
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            dashboardControl.ObjectDataSourceLoadingBehavior = DevExpress.DataAccess.DocumentLoadingBehavior.LoadAsIs;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Dashboard dashboard = CreateDashboard();
+            dashboardControl.Dashboard = dashboard;
+        }
+        private void dashboardControl_AsyncDataLoading(object sender, DataLoadingEventArgs e)
+        {
+            if (e.DataSourceName == "MyDataSource")
+                e.Data = Data.CreateData();
+        }
+
+        private Dashboard CreateDashboard()
+        {
+            Dashboard dashboard = new Dashboard();
+            DashboardObjectDataSource objectDataSource = new DashboardObjectDataSource("MyDataSource");
+            objectDataSource.DataSource = typeof(Data);
+            objectDataSource.DataMember = "CreateData";
+            dashboard.DataSources.Add(objectDataSource);
+
+            // Creates a Pie dashboard item that displays a percentage of quantity summary for top 3 sales persons.
+            PieDashboardItem pies = new PieDashboardItem();
+            pies.DataSource = dashboard.DataSources[0];
+            Dimension salesPersonArgument = new Dimension("SalesPerson");
+            Measure quantity = new Measure("Quantity");
+            pies.Arguments.Add(salesPersonArgument);
+            salesPersonArgument.TopNOptions.Enabled = true;
+            salesPersonArgument.TopNOptions.Count = 3;
+            salesPersonArgument.TopNOptions.Measure = quantity;
+            pies.LabelPosition = PointLabelPosition.Inside;
+            pies.Values.Add(quantity);
+
+            // Creates a Grid dashboard item that displays sales persons and quantities.
+            GridDashboardItem grid = new GridDashboardItem();
+            grid.DataSource = dashboard.DataSources[0];
+            grid.Columns.Add(new GridDimensionColumn(new Dimension("SalesPerson")));
+            grid.Columns.Add(new GridMeasureColumn(new Measure("Quantity")));
+
+            dashboard.Items.AddRange(pies, grid);
+            return dashboard;
+        }
+
+        private void simpleButton_Click(object sender, RoutedEventArgs e)
+        {
+            dashboardControl.ReloadData();
+        }
+    }
+}
